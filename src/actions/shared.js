0e760fe7ce10f69607e29abf584f4fa62842f52d
Ben Flores <beflore@microsoft.com>
2019-02-02 12:33:20 -0800

    Import newly created action creators, dispatch them inside the .then() method of the getInitialData API call

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,9 +1,14 @@
 import { getInitialData } from "../utils/api";
+import { receiveUsers } from "../actions/users";
+import { receiveTweets } from "../actions/tweets";
 
 export function handleInitialData() {
   // this function uses the Redux Thunk pattern,
   // because we want to make an aynchronous request inside this function
   return dispatch => {
-    return getInitialData().then(({ users, tweets }) => {});
+    return getInitialData().then(({ users, tweets }) => {
+      dispatch(receiveUsers(users));
+      dispatch(receiveTweets(tweets));
+    });
   };
 }
