b89423c854f5f0f61fa6514c164e05c2fe4e09cf
Ben Flores <beflore@microsoft.com>
2019-02-02 12:43:15 -0800

    Create AUTHED_ID constant, import setAuthedUser action creator, implement it inside handleInitialData

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,9 @@
 import { getInitialData } from "../utils/api";
 import { receiveUsers } from "../actions/users";
 import { receiveTweets } from "../actions/tweets";
+import { setAuthedUser } from "../actions/authedUser";
+
+const AUTHED_ID = "tylermcginnis";
 
 export function handleInitialData() {
   // this function uses the Redux Thunk pattern,
@@ -9,6 +12,7 @@ export function handleInitialData() {
     return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveUsers(users));
       dispatch(receiveTweets(tweets));
+      dispatch(setAuthedUser(AUTHED_ID));
     });
   };
 }
