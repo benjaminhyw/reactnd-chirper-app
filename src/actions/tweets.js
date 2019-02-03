3129f75e3e1f144b3c7564fead0ad5f57bc8ddfd
Ben Flores <beflore@microsoft.com>
2019-02-03 10:20:14 -0800

    Create toggleTweet action creator

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,9 +1,21 @@
+import { saveLikeToggle } from "../utils/api";
+
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS";
+export const TOGGLE_TWEET = "TOGGLE_TWEET";
 
 //this is an ActionCreator
 export function receiveTweets(tweets) {
   return {
     type: RECEIVE_TWEETS,
     tweets
+  };
+}
+
+function toggleTweet({ id, authedUser, hasLiked }) {
+  return {
+    type: TOGGLE_TWEET,
+    id,
+    authedUser,
+    hasLiked
   };
 }
