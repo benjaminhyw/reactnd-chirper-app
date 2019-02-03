23d8a0da0d9f0d95417ee5bbfde4804f497a0400
Ben Flores <beflore@microsoft.com>
2019-02-03 10:44:10 -0800

    Create handleChange & handleSubmit methods inside NewTweet

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,6 +1,25 @@
 import React, { Component } from "react";
 
 export default class NewTweet extends Component {
+  state = {
+    text: ""
+  };
+  handleChange = e => {
+    const text = e.target.value;
+    this.setState(() => ({
+      text
+    }));
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const { text } = this.state;
+    //todo: Add tweet to store
+
+    console.log("New Tweet: ", text);
+    this.setState(() => {
+      text: "";
+    });
+  };
   render() {
     return <div>New Tweet</div>;
   }
