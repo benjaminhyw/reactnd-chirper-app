46ab93587824ed503b2115f1ac931b97ac2f754e
Ben Flores <beflore@microsoft.com>
2019-02-03 10:51:49 -0800

    Build the UI for the Compose New Tweet page

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -21,6 +21,27 @@ export default class NewTweet extends Component {
     });
   };
   render() {
-    return <div>New Tweet</div>;
+    const { text } = this.state;
+    //   todo: Redirect to / if submitted
+    const tweetLeft = 280 - text.length;
+
+    return (
+      <div>
+        <h3 className="center">Compose New Tweet</h3>
+        <form className="new-tweet" onSubmit={this.handleSubmit}>
+          <textarea
+            placeholder="What's happening?"
+            value={text}
+            onChange={this.handleChange}
+            className="textarea"
+            maxLength={280}
+          />
+          {tweetLeft <= 100 && <div className="tweet-length">{tweetLeft}</div>}
+          <button className="btn" type="submit" disabled={text === ""}>
+            Submit
+          </button>
+        </form>
+      </div>
+    );
   }
 }
